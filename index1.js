c564795e21f171bbbff6a418811d4d036d5e27f7
VladChaka <super.chuhunov@yandex.com>
2018-05-16 15:07:54 +0300

    firs stap

---
+++ index1.js
@@ -0,0 +1,12 @@
+let server = require("./Server"),
+    router = require("./router"),
+    requestHandlers = require('./requestHandlers'),
+    handle = {};
+
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+
+server.start(router.route, handle);
